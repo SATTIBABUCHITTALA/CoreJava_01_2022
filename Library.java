7771712461dd6aa8c2411e354645708e88e0e7d2
SATTIBABUCHITTALA <57794381+SATTIBABUCHITTALA@users.noreply.github.com>
2022-02-01 23:24:59 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,33 @@
+
+
+
+class Library{
+
+public static void main(String args[]){
+
+boolean flag =false;
+
+int i=10;
+int j=3;
+float k=(float)i/j;
+double d=(double)i/j;
+char c='A';
+byte b=127;
+
+long l=i/j;
+
+
+
+System.out.println("Character Value "+c);
+System.out.println("Byte Value "+b);
+System.out.println("Double Value "+d);
+System.out.println("float value "+k);
+System.out.println("flag Value "+flag);
+System.out.println("long Value "+l);
+
+
+
+}
+
+
+}
---
+++ StaticClass.java
@@ -0,0 +1,15 @@
+public class StaticClass{
+
+static{
+System.out.println("Static Block is Declared");
+
+}
+public static void main(String args[]){
+
+System.out.println("Welcome");
+
+}
+
+
+
+}
