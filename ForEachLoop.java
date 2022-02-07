3baf648c92eb129b77bedd6715b1dcb0e01266fd
SATTIBABUCHITTALA <57794381+SATTIBABUCHITTALA@users.noreply.github.com>
2022-02-07 22:26:58 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,28 @@
+public class ForEachLoop{
+
+public static void main(String args[]){
+
+String [] list={"Ashok","Ravi","Ramesh"};
+
+System.out.println("For Loop");
+for(int i=0;i<list.length;i++)
+System.out.println(list[i]); // 
+
+System.out.println("For Each Loop");
+
+for(String i:list)
+System.out.println(i);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
---
+++ WhileLoopDemo.java
@@ -0,0 +1,23 @@
+public class WhileLoopDemo{
+
+public static void main(String args[]){
+
+int i=0;
+
+while(i<=10){
+
+System.out.println("i value"+ i);
+i++; //11
+}//0,1,2,3,4,5,6,7,8,9,10
+//11
+do{
+
+System.out.println("i value"+ i);     //11
+
+}while(i>13); //i=11, i>13 false;
+
+}
+
+
+
+}
